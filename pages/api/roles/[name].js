9498cf88db21d4cb1634fa103a9c4d2c8728281c
tulusibrahim <tulusibrahim20@gmail.com>
2022-04-22 16:02:11 +0700

    change to nextjs

--- pages/api/roles/[name].js
+++ pages/api/roles/[name].js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
     let data = await fetch(`https://mapi.mobilelegends.com/hero/list?type=${req.query.name}&language=en`)
     let json = await data.json()
+
     res.send(json)
 }
