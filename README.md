a84f0195b8820664d717f3c2fc7089a4afb1fecc
tulusibrahim <tulusibrahim20@gmail.com>
2022-04-22 13:54:11 +0700

    fix: add route

--- README.md
+++ README.md
@@ -8,17 +8,46 @@ A brief description of what this project does and who it's for
 
 Base URL: ```https://unofficial-mobile-legends-api.vercel.app```
 
-#### Get Detail
+#### Get All Heroes
 
 ```http
-  GET /api/index/:id
+  GET /heroes
+```
+
+| Parameter | Type      | Description                       |
+| :-------- | :-------- | :------------------------ |
+| `heroes`  | `string`  | Get list of all heroes |
+
+#### Get Hero Detail
+
+```http
+  GET /hero/:id
 ```
 
 | Parameter | Range    | Type     | Description                       |
 | :-------- | :------- | :--------| :------------------------ |
-| `id`      | 1-115    | `string` | **Required**. Id of hero to fetch |
+| `id`      | 1-115    | `string` | Id of hero to fetch |
+
+#### Get All Roles
+
+```http
+  GET /roles
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :--------| :------------------------ |
+| `roles`   | `string` | List of All roles |
+
+#### Get Hero by Roles
+
+```http
+  GET /roles/:name
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :--------| :------------------------ |
+| `name`    | `string` | List of hero by roles |
 
 Credits
 
 Copyright © 2022 Tulus Ibrahim
-
--- api/index.js
+++ api/index.js
@@ -2,21 +2,27 @@ const express = require('express')
 const router = express.Router()
 let axios = require('axios')
 
-router.get('/hero/:id', async (req, res) => {
+router.get('/heroes/', async (req, res) => {
+    let data = await axios.get(`${process.env.URL}/hero/list?language=${lang ? lang : 'en'}`)
+    res.send(data.data)
+})
+
+router.get('/hero/:id/', async (req, res) => {
     // last id == 115
     let heroID = req.params.id
-    let data = await axios.get(`${process.env.URL}/hero/detail?id=${heroID}&language=en`)
+    let data = await axios.get(`${process.env.URL}/hero/detail?id=${heroID}&language=${lang ? lang : 'en'}`)
     res.send(data.data)
 })
 
-router.get('/heroes', async (req, res) => {
-    let data = await axios.get(`${process.env.URL}/hero/list?language=en`)
-    res.send(data.data)
+router.get('/roles', async (req, res) => {
+    let tes = await axios.post(process.env.URL_HERO_LIST, { "lang": "en", "language": "en" })
+    res.send(tes.data.data.channel_data)
 })
 
-
-// router.get('/role', async () => {
-
-// })
+router.get('/roles/:name', async (req, res) => {
+    let name = req.params.name
+    let data = await axios.get(`${process.env.URL}/hero/list?type=${name}&language=en`)
+    res.send(data.data)
+})
 
 module.exports = router
