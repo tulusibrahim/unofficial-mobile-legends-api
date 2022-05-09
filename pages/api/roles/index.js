deda00e0630e1e232d22d0bd610d9f94a55eb212
tulusibrahim <tulusibrahim20@gmail.com>
2022-05-09 12:05:33 +0700

    change url to env var

--- pages/api/roles/index.js
+++ pages/api/roles/index.js
@@ -1,12 +1,14 @@
 export default async function handler(req, res) {
-    let data = await fetch(process.env.HERO_LIST, {
+    let param = {
         "headers": {
             "accept": "application/json",
             "content-type": "application/json",
         },
         "body": "{\"lang\":\"en\",\"language\":\"en\"}",
         "method": "POST",
-    });
+    }
+
+    let data = await fetch(process.env.HERO_LIST, param);
     let json = await data.json()
     console.log(json)
     res.send(json.data.channel_data)
