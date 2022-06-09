e43861b64558bf8a52c0ef24d3b600af9ebd3cd4
tulusibrahim <tulusibrahim20@gmail.com>
2022-06-09 20:32:09 +0700

    fix: add default route in /api

---
+++ pages/api/index.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+    res.json({
+        message: 'Please head to the documentation for further usage.',
+        link: 'https://github.com/tulusibrahim/unofficial-mobile-legends-api'
+    })
+}
